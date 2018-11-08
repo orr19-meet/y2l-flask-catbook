cb54927cc0ca5fd02527d0bdf4aa5c409ea13e58
aboud20-meet <aboud20@meet.mit.edu>
2018-11-08 20:07:15 +0200

    fghj

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from database import get_all_cats
+from database import get_all_cats ,get_cat_by_id
 
 app = Flask(__name__)
 
@@ -8,6 +8,12 @@ app = Flask(__name__)
 def catbook_home():
     cats = get_all_cats()
     return render_template("home.html", cats=cats)
+
+@app.route('/cats/<int:n>')
+def catbook_cat(n):
+	cat=get_cat_by_id(n)
+	return render_template("cat.html", cat=cat)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
